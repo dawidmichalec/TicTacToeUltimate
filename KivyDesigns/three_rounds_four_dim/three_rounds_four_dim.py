19cb74d0bda09b0e759674d4114528b8a79b17b2
Dawid Michalec <michalecdawid95@gmail.com>
2024-01-27 15:30:31 +0100

    Added screens to three_rounds_four_dim. Fixed an issue with result labels.

---
+++ KivyDesigns/three_rounds_four_dim/three_rounds_four_dim.py
@@ -0,0 +1,8 @@
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.menu.menu import MDDropdownMenu
+from kivy.factory import Factory
+from kivy.app import App
+
+
+class ThreeRoundsFourDim(Screen):
+    pass
