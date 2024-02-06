6a4ae183f0aec9e8f8f8e6c64067ca694b0dd9fa
Dawid Michalec <michalecdawid95@gmail.com>
2024-02-06 20:56:47 +0100

    Added screens to three_rounds_five_dim.

---
+++ KivyDesigns/three_rounds_five_dim/three_rounds_five_dim_result.py
@@ -0,0 +1,28 @@
+from kivy.uix.screenmanager import Screen
+from kivy.factory import Factory
+from kivymd.uix.behaviors.hover_behavior import HoverBehavior
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
+from kivy.uix.modalview import ModalView
+
+
+class ThreeRoundsFiveDimResult(Screen):
+
+    def on_enter(self):
+
+        Factory.get('ThreeRoundsFiveDimResultPopup')().open()
+
+
+
+class ResultButton(Button, HoverBehavior):
+
+    background = ListProperty((120/255, 120/255, 120/255, 1))
+    color = ListProperty((1, 1, 1, 1))
+
+    def on_enter(self):
+        self.background = (1, 222/255, 89/255, 1)
+        self.color = (0, 0, 0, 1)
+
+    def on_leave(self):
+        self.background = (120/255, 120/255, 120/255, 1)
+        self.color = (1, 1, 1, 1)
